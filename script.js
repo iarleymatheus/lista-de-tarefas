1349d1a356438ce0e34ae1a03664f61388fcf670
iarleymatheus <yarley.matheus@hotmail.com>
2021-12-25 20:05:01 -0300

    funcao apagar tudo adicionada

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 const input = document.querySelector("#add");
 const btn = document.querySelector("#btn");
 const list = document.querySelector("#list");
+const btnDel = document.querySelector("#btnDel")
 var el = document.getElementById('li');
+var apagarIndividual = document.getElementById('#btnapagar')
 
-btn.onclick = function(){
-   var txt = input.value;
-   if (txt == ''){
-     alert('Digite a Tarefa!');
+
+btn.onclick = function () {
+  var txt = input.value;
+  if (txt == '') {
+    alert('Digite a Tarefa!');
   }
-  else{
-      li = document.createElement('li');
-      li.innerHTML = txt;
-      list.insertBefore(li,list.childNodes[0]) ;
-      input.value = '';
-
-}
+  else {
+    li = document.createElement('li');
+    li.innerHTML =txt;
+    list.insertBefore(li, list.childNodes[0]);
+    input.value = '';
+    
+  }
 };
 
 
-list.onclick = function(ev){
-  if(ev.target.tagName == 'LI'){
+list.onclick = function (ev) {
+  if (ev.target.tagName == 'LI') {
     ev.target.classList.toggle('checked');
   }
 };
 
+
+function apagarTudo() {
+  list.innerHTML = "";
+}
